8b97def2588539fc4ffbff957db65434181c5707
obyte1 <30421073+obyte1@users.noreply.github.com>
2019-09-26 10:36:43 +0100

    Add files to Github

---
+++ App Data/sqlcon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Pharmcacy1.App_Data
+{
+    class sqlcon
+    {
+        public static SqlConnection con = null;
+        public void Connection_Today()
+        {
+            con = new SqlConnection(@"Data Source=SIDSYS-LAP-22\SQLEXPRESS;Initial Catalog=mypharmacy;Integrated Security=True");
+            con.Open();
+        }
+
+
+    }
+}
---
+++ App Data/sqlconclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ConnectionString
+{
+    private static string _connectionString = (@"Data Source=SIDSYS-LAP-22\SQLEXPRESSS;Initial Catalog=mypharmacy;Integrated Security=True");
+
+    public ConnectionString()
+	{
+	}
+    public static string CONNECTIONSTRING
+    {
+        get { return _connectionString; }
+    }
+}
