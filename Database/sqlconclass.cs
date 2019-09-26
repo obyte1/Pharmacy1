c4eb0e89faec8f1d0b3a7681bf6f1713e7459227
obyte1 <30421073+obyte1@users.noreply.github.com>
2019-09-26 10:46:37 +0100

    Add files via upload

---
+++ Database/sqlconclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.data;
+using System.data.sqlclient;
+
+
+
+public class ConnectionString
+{
+    private static string _connectionString = @"Data Source=SIDSYS-LAP-22\\sqlexpress;Initial Catalog=mypharmacy;Integrated Security=True";
+
+    public ConnectionString()
+	{
+	}
+    public static string CONNECTIONSTRING
+    {
+        get { return _connectionString; }
+    }
+}
